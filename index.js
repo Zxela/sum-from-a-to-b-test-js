943318c2f4cfcfa79f1cb079c474e6009638d388
Alex Zvaniga <Zxela@users.noreply.github.com>
2018-08-02 14:58:47 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN >= toN) {
+    return toN;
+  }
+  return fromN + sum(fromN + 1, toN);
 }
-
+console.log(sum(2, 5));
 module.exports = sum;
